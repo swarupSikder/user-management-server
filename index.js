fd77bcaa5222ddae72cfef899c86c9e5f716beaf
swarupSikder <swarupfx20@gmail.com>
2025-10-04 19:30:52 +0600

    add and show users api added

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('User_Management Server is running...')
 })
+
+const users = [
+    {id: 1, name: 'Sabana', email: 'sabana@gmail.com'},
+    {id: 2, name: 'Sabnoor', email: 'sabnoor@gmail.com'},
+    {id: 3, name: 'Sabila', email: 'sabila@gmail.com'},
+]
+
+
+
+
+//-------------//
+//   get user  //
+//-------------//
+app.get('/users', (req, res)=>{
+    res.send(users);
+})
+
+
+//--------------//
+//   post user  //
+//--------------//
+app.post('/users', (req, res)=>{
+    res.send(users);
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`User-Management app listening on port ${port}`)
